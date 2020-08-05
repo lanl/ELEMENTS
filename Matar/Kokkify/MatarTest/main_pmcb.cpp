a4d338eeec5a11f7e6cef9ed0a40eab25a4ff8ff
esn <esn@lanl.gov>
2020-08-05 17:16:45 -0600

    Added add_executable line to compile main_pmcb (source file that will contain perf. benchmarks for MATAR's Kokkos-specific data types).

---
+++ Matar/Kokkify/MatarTest/main_pmcb.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <chrono>
+#include "pseudo_mesh_cpu_benchmark.hpp"
+
+int main() {
+    Kokkos::initialize();
+    {
+    }
+    Kokkos::finalize();
+
+    return 0;
+}
---
+++ Matar/Kokkify/MatarTest/pseudo_mesh_cpu_benchmark.hpp
@@ -0,0 +1,50 @@
+#ifndef PSEUDO_MESH_CPU_BENCHMARK
+#define PSEUDO_MESH_CPU_BENCHMARK
+
+#include "matar2.h"
+
+using TeamPolicy = Kokkos::TeamPolicy<Kokkos::Cuda>;
+
+class pmcb {
+    public:
+        size_t size1;
+        size_t size2;
+        size_t size3;
+        size_t size4;
+        size_t size5;
+        size_t size6;
+
+        FArrayKokkos<real_t>  fak;
+        FMatrixKokkos<real_t> fmk;
+        CArrayKokkos<real_t>  cak;
+        CMatrixKokkos<real_t> cmk;
+
+        // Default constructor
+        pmcb();
+
+        // 1D initialization function
+        void init(size_t dim1);
+
+        // 2D initialization function
+        void init(size_t dim1, size_t dim2);
+
+        // 3D initialization function
+        void init(size_t dim1, size_t dim2, size_t dim3);
+
+        // 4D initialization function
+        void init(size_t dim1, size_t dim2, size_t dim3, size_t dim4);
+
+        // 5D initialization function
+        void init(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
+                  size_t dim5);
+
+        // 6D initialization function
+        void init(size_t dim1, size_t dim2, size_t dim3, size_t dim4,
+                  size_t dim5, size_t dim6);
+
+        // Destructor
+        ~pmcb();
+
+};
+
+#endif

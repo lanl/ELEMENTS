6a0c2dbec7da796fd7ad2e39200abe990ffcf0b9
Jan Filipe Kiviaho <jfk115@msstate.edu>
2022-07-29 14:42:03 -0500

    Prepare gh-pages branch for auto-publish

--- docs/CMakeFiles/Doxygen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/html/index.html"
-  "CMakeFiles/Doxygen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Doxygen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- docs/doxygen/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['customization_20points_451',['Customization Points',['../md__home_jan_packages_elements_include_gmock_internal_custom_README.html',1,'(Global Namespace)'],['../md__home_jan_packages_elements_include_gtest_internal_custom_README.html',1,'(Global Namespace)']]]
-];

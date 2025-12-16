fc7e0d50450f6e1910319d1013598c236fda9128
Jacob Moore <jacoblinleymoore@gmail.com>
2025-12-16 11:42:49 -0600

    BUG: Chasing build issues

---
+++ examples/average/src/average.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mesh.h"
+#include "mesh_inputs.h"
+#include "mesh_io.h"
+#include "state.h"
+
+
+int main(int argc, char** argv) {
+
+MATAR_INITIALIZE(argc, argv);
+{ // MATAR scope
+    std::cout<<"Hello, Average Example!"<<std::endl;
+    
+    
+    
+    
+    
+} // end MATAR scope
+MATAR_FINALIZE();
+
+return 0;
+}

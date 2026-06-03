d6564693ad3c1ffb1a6f1ed72ae2783db06b70e2
Jacob Moore <jacoblinleymoore@gmail.com>
2026-06-03 13:10:39 -0500

    Backing up, swapping to other work, WIP

--- examples/xPBD/src/xPBD_example.cpp
+++ examples/xPBD/src/xPBD_example.cpp
@@ -95,38 +95,18 @@ int main(int argc, char** argv)
         // Mesh initialized, now implement the xPBD solver. 
 
         XpbdSolverConfig solver_config;
-        solver_config.poly_order = poly_order;
         solver_config.num_timesteps = 200;
         solver_config.num_solver_iterations = 10;
         solver_config.num_force_ramp_steps = 10;
         solver_config.num_outputs = num_outputs;
         solver_config.timestep_dt = 5.0e-4;
         solver_config.velocity_damping_factor = 0.05;
-        solver_config.dirichlet_plane_origin[0] = origin[0];
-        solver_config.dirichlet_plane_origin[1] = origin[1];
-        solver_config.dirichlet_plane_origin[2] = origin[2];
-        solver_config.dirichlet_plane_normal[0] = 0.0;
-        solver_config.dirichlet_plane_normal[1] = 0.0;
-        solver_config.dirichlet_plane_normal[2] = 1.0;
-        solver_config.surface_traction_plane_normal[0] = 0.0;
-        solver_config.surface_traction_plane_normal[1] = 0.0;
-        solver_config.surface_traction_plane_normal[2] = 1.0;
-        solver_config.gravity_vector[0] = 0.0;
-        solver_config.gravity_vector[1] = 1.0;
-        solver_config.gravity_vector[2] = -9.81;
         solver_config.material.density = 1000.0;
         solver_config.material.youngs_modulus = 1.0e4;
         solver_config.material.poissons_ratio = 0.3;
         solver_config.material.compliance = 1.0 / solver_config.material.youngs_modulus;
 
-        XpbdSolverState solver_state;
-        initialize_xpbd_solver(
-            final_mesh,
-            final_node_coords,
-            node_communication_plan,
-            element_communication_plan,
-            solver_state,
-            solver_config);
+    
 
         print_inverse_lumped_mass_stats(solver_state, final_mesh, rank);
 
@@ -148,28 +128,6 @@ int main(int argc, char** argv)
                       << std::endl;
         }
 
-        double max_displacement = 0.0;
-        double local_max_displacement = 0.0;
-        solver_state.node_positions.update_host();
-        solver_state.reference_node_positions.update_host();
-        for (size_t node_gid = 0; node_gid < final_mesh.num_owned_nodes; ++node_gid) {
-            for (int spatial_dim = 0; spatial_dim < 3; ++spatial_dim) {
-                const double displacement = solver_state.node_positions.host(node_gid, spatial_dim)
-                    - solver_state.reference_node_positions.host(node_gid, spatial_dim);
-                local_max_displacement = std::max(local_max_displacement, std::fabs(displacement));
-            }
-        }
-        MPI_Allreduce(
-            &local_max_displacement,
-            &max_displacement,
-            1,
-            MPI_DOUBLE,
-            MPI_MAX,
-            MPI_COMM_WORLD);
-
-        if (rank == 0) {
-            std::cout << "Maximum nodal displacement magnitude: " << max_displacement << std::endl;
-        }
 
         MPI_Barrier(MPI_COMM_WORLD);
 
